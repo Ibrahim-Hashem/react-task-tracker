a3ad4b059d4f538e0dc57537b1931882e67c52cc
Ibrahim Hashem <ibrahimsgh6@hotmail.co.uk>
2021-03-15 21:57:31 +0000

    this is the tasks component that was made and task element was added to app.js

---
+++ src/components/Tasks.js
@@ -0,0 +1,12 @@
+// import PropTypes from 'prop-types'
+const Tasks = ({tasks}) => {
+    return (
+        <>
+            {tasks.map((task)=>(
+                <h3 key={task.id}>{task.text}</h3>
+            ))}   
+        </>
+    )
+}
+
+export default Tasks
