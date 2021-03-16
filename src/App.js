e7842c0aa21a6bfaa39f4dddc3703aab4c0c50d8
Ibrahim Hashem <ibrahimsgh6@hotmail.co.uk>
2021-03-16 11:38:32 +0000

    task delete function created

--- src/App.js
+++ src/App.js
@@ -23,12 +23,18 @@ function App() {
         reminder: false,
     }
 ])
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task)=> task.id !== id))
+}
+
   return (
     <div className="container">
       <Header title='Task Tracker'/>
-      <Tasks tasks = {tasks}/>
+      <Tasks tasks = {tasks} onDelete={deleteTask}/>
     </div>
   );
+
+
 }
 
 export default App;
